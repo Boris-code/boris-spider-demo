fc6da4ec3c4a88318b1d3b3dfd9a2889fde7372d
刘博 <564773807@qq.com>
2020-07-13 13:32:11 +0800

    添加分布式爬虫示例

---
+++ spider_demo/items/spider_test_item.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2020-07-13 13:04:43
+---------
+@summary:
+---------
+@author: liubo
+'''
+
+from spider import Item
+
+
+class SpiderTestItem(Item):
+    def __init__(self, *args, **kwargs):
+        # self.id = None  # type : int(10) unsigned | allow_null : NO | key : PRI | default_value : None | extra : auto_increment | column_comment : 
+        self.title = None  # type : varchar(255) | allow_null : YES | key :  | default_value : None | extra : | column_comment :
---
+++ spider_demo/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2020-07-12 22:25:43
+---------
+@summary: 爬虫入口
+---------
+@author: liubo
+"""
+
+from parsers import *
+if __name__ == "__main__":
+    spider = test_parsers.TestParsers(table_folder="test:test1")
+    spider.start()
